64f80258c94fd3638a95995d535cd3278602c67e
Siarhei_Shestak <Siarhei_Shestak@epam.com>
2021-03-12 07:49:42 +0300

    first commit

---
+++ GetStartedDebugging/GetStartedDebugging/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GetStartedDebugging
+{
+    class ArrayExample
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = { 's', 'i', 'a', 'r', 'h', 'e', 'i', ',', 's', 'h', 'e', 's', 't', 'a', 'k' };
+            String name = "";
+            int[] a = new int[15];
+            for(int i=0;i<letters.Length; i++)
+            {
+                name += letters[i];
+                a[i] = i + 1;
+                SendMessage(name, a[i]);
+            }
+            Console.ReadKey();
+        }
+
+        static void SendMessage(string name, int msg)
+        {
+            Console.WriteLine("Hello," + name + "! Count " + msg);
+        }
+    }
+
+}
