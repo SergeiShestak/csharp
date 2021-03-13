061d4e44ea6638fdd760cfe056363c5335a376c5
Siarhei_Shestak <Siarhei_Shestak@epam.com>
2021-03-13 10:34:15 +0300

    second edition

---
+++ MaxLengthString2/MaxLengthString2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MaxLengthString2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            Counter counter = new Counter();
+            Messanger messanger = new Messanger();
+            messanger.printMessage(counter.countLength(reader.readArgs(args))); 
+
+        }
+    }
+
+    class Reader
+    {
+        public string readArgs(String[] args)
+        {
+            string argc = args[0];
+            return argc;
+        }
+    }
+
+    class Messanger
+    {
+
+        public void printMessage(int output)
+        {
+            Console.WriteLine($"Maximum length of string is:{output}");
+        }
+    }
+
+    class Counter
+    {
+        public int countLength(String args)
+        {
+            
+            int output = 0;
+            int tempOutput = 0;
+            char tempChar = (char)0;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] != tempChar)
+                {
+                    tempOutput++;
+                }
+                else
+                {
+                    tempOutput = 0;
+                }
+                if (output < tempOutput)
+                {
+                    output = tempOutput;
+                }
+                tempChar = args[i];
+
+            }
+            return output;
+        }
+    }
+}
